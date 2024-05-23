abdf27fcab2250c1e8c99732cadb77b8aed472d0
RomaRoma <Bigbublik@icloud.com>
2024-05-23 23:08:42 +0200

    Initial Commit

---
+++ SwiftUI X CoordinatorPattern/SwiftUI_X_CoordinatorPatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_X_CoordinatorPatternApp.swift
+//  SwiftUI X CoordinatorPattern
+//
+//  Created by Roman on 23.05.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_X_CoordinatorPatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
